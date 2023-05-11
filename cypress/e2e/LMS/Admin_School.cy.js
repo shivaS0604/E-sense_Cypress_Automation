c46111a122b147116e06cac472d7813d4b1afd1b
shivaS0604 <sivaguru.s@testyantra.com>
2023-05-11 16:01:47 +0530

    resolved uncaught exception

--- cypress/e2e/LMS/Admin_School.cy.js
+++ cypress/e2e/LMS/Admin_School.cy.js
@@ -3,7 +3,7 @@ describe("Admin School Validation", function () {
     before(function () {
         cy.visit(Cypress.env("url"))
         cy.fixture("LMS/Credentials").then(function (validAdminLoginData) {
-          cy.adminLogin(validAdminLoginData.username, validAdminLoginData.password)
+          cy.adminLogin(validAdminLoginData.username,validAdminLoginData.password)
         })
       })
 
