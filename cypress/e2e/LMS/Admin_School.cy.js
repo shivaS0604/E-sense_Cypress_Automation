1179313554d403e206818ba1fe5737c9470608dd
shivaS0604 <sivaguru.s@testyantra.com>
2023-05-11 11:53:51 +0530

    last Commit

--- cypress/e2e/LMS/Admin_School.cy.js
+++ cypress/e2e/LMS/Admin_School.cy.js
@@ -1,13 +1,5 @@
 describe("Admin School Validation", function () {
 
-  beforeEach(function () {
-    cy.visit(Cypress.env("url"))
-    cy.viewport(1920, 1080)
-    cy.fixture("LMS/Credentials").then(function (validAdminLoginData) {
-      cy.adminLogin(validAdminLoginData.username, validAdminLoginData.password)
-    })
-  })
-
     before(function () {
         cy.visit(Cypress.env("url"))
         cy.fixture("LMS/Credentials").then(function (validAdminLoginData) {
