b28acc71f00debcc35771bc0afd6347e6435affa
Manoj kumar <manuvirat772@gmail.com>
2023-05-12 17:38:26 +0530

    12/5/23_CommitDone_Manoj

--- cypress/e2e/LMS/Admin_Reports.cy.js
+++ cypress/e2e/LMS/Admin_Reports.cy.js
@@ -1,5 +1,5 @@
-/// <reference types = "Cypress" />
 const ReportDashboardPage=require('../../support/pageObjects/LMS/adminReportPage')
+
 describe('Validate Admin Edited the Gradebook Template Successfully',function(){
 
     before(function(){
@@ -12,12 +12,43 @@ describe('Validate Admin Edited the Gradebook Template Successfully',function(){
 
     it('Verify that School Admin can Edit the TopSchool Gradebook Template',function(){
         ReportDashboardPage.getAdminReportsSideMenubarReportTab().click()
-      //  cy.get('div.side-nav-bar ').invoke('show').contains('Reports').click()
-        cy.get('[class="content-popover-reports"]').eq(0).should('be.visible').click({force:true})
+        ReportDashboardPage.getAdminReportsStudentGradebookTab().click()
         cy.wait(2000)
-        cy.contains('Student Gradebook').should('have.text','Student Gradebook')
-        cy.xpath('//p[contains(text(),"Grade 1")]/ancestor::tr//img[@aria-label="Edit"]').should('be.visible').click()
-        cy.contains('Basic Template Details').should('have.text','Basic Template Details')
+        ReportDashboardPage.getAdminReportsVerifyStudentGradebookText().should('have.text','Student Gradebook')
+        ReportDashboardPage.getAdminReportsGradeEditButton().click()
+    
+        ReportDashboardPage.getAdminReportsVerifyBasicTemplateDetailsText().should('have.text','Basic Template Details')
+        ReportDashboardPage.getCreateNewTemplateGradeDropdown().should('have.text','Grade 1')
+        ReportDashboardPage.getCreateNewTemplateSectionDropdown().should('have.text','A')
+        ReportDashboardPage.getCreateNewTemplateNumberOftermsDropdown().click()
+        ReportDashboardPage.getCreateNewTemplateSelectNumberOftermsValue().click()
+        ReportDashboardPage.getCreateNewTemplatePublishGradebookInDropdown().click()
+        ReportDashboardPage.getCreateNewTemplateSelectPublishGradebookInValue().click({force:true})
+        ReportDashboardPage.getCreateNewTemplateAddTestTypeButton().click()
+        cy.get(':nth-child(4) > .MuiButton-root').click()
+        ReportDashboardPage.getCreateNewTemplateTermDropdown().click()
+        ReportDashboardPage.getCreateNewTemplateTermCheckbox().click()
+        cy.get('body').click()
+        ReportDashboardPage.getCreateNewTemplateTestTypeDropdown().click()
+        ReportDashboardPage.getCreateNewTemplateSelectTestTypeValue().click()
+        ReportDashboardPage.getCreateNewTemplateMaxMarksDropdown().click()
+        ReportDashboardPage.getCreateNewTemplateSelectMaxMarksValue().click()
+
+        ReportDashboardPage.getCreateNewTemplateAddSubjectButton().click()
+        ReportDashboardPage.getCreateNewTemplateSubjectDropdown().click()
+        ReportDashboardPage.getCreateNewTemplateSelectSubjectvalue().click()
+
+        ReportDashboardPage.getCreateNewTemplateAddTheoryAndPracticleButton().click()
+        ReportDashboardPage.getAddTheoryAndPracticleTestTypeDropdown().click({force:true})
+        ReportDashboardPage.getAddTheoryAndPracticleTestTypeValue().click()
+        ReportDashboardPage.getCreateNewTemplateTheoryTextfield().should('have.value','15')
+        ReportDashboardPage.getCreateNewTemplatePracticleTextfield().click().type('6')
+
+        ReportDashboardPage.getCreateNewTemplateAddActivityTextfield().type('Drawing')
+       ReportDashboardPage.getCreateNewTemplateAddPrincipleSignatureButton().click()
+       ReportDashboardPage.getCreateNewTemplateVerifyAddSignatureTxt().should('have.value','Add Signature')
+       ReportDashboardPage.getCreateNewTemplateUploadImage().click()
 
     })
+    //author - manoj
 })
