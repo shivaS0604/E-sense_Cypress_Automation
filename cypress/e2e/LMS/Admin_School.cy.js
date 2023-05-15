15aaf2d14282ddf955b927d8b5b3245276155645
AkshayGowda52 <akshayga52@gmail.com>
2023-05-15 18:20:37 +0530

    commit ak

--- cypress/e2e/LMS/Admin_School.cy.js
+++ cypress/e2e/LMS/Admin_School.cy.js
@@ -1,5 +1,6 @@
 const adminschoolpage = require('../../support/pageObjects/LMS/admin_Schoolpage.js')
 const academicsetuppage = require('../../support/pageObjects/LMS/academicSetUpPage.js')
+const gradeAndSubjectPage = require('../../support/pageObjects/LMS/academicSetUpGrade&SubjectPage.js')
 
 describe("Admin School Validation", function () {
 
@@ -11,12 +12,6 @@ describe("Admin School Validation", function () {
         })
         cy.fixture("LMS/admin_School").as('academicSetUp')
     })
-      
-    // before(function () {
-    //     cy.fixture("LMS/Credentials").then(function (validAdminLoginData) {
-    //       cy.adminLogin(validAdminLoginData.username, validAdminLoginData.password)
-    //     })
-    //   })
 
       it('admin school 001 Verify that School admin can add the new academic year successfully',function () {
         cy.wait(2000)
@@ -28,20 +23,18 @@ describe("Admin School Validation", function () {
         academicsetuppage.getAddNewBttn().click()
         academicsetuppage.getAddAcademicSetUpPopUpTittle().contains("Academic Year")
         academicsetuppage.getAddAcademicPopUpSelectYearDropdownBttn().click()
-        academicsetuppage.getAddAcademicPopUpSelectYearDropdownLists().click()
+        academicsetuppage.getAddAcademicPopUpSelectYearDropdownLists().eq(0).click()
         academicsetuppage.getAddAcademicSetUpPopUpStartDateCalenderIcon().eq(0).click()
         academicsetuppage.getAddAcademicSetUpPopUpStartDateCalenderArrowRightIcon().click()
         academicsetuppage.getAddAcademicSetUpPopUpCalenderDates().contains('30').click()
         academicsetuppage.getAddAcademicSetUpPopUpStartDateCalenderIcon().eq(1).click()
-        for(let i=0;i<=5;i++){
-          academicsetuppage.getAddAcademicSetUpPopUpStartDateCalenderArrowRightIcon().as('RightArrowBtn')
-          cy.get('@RightArrowBtn').click()
-        }
+        academicsetuppage.getAddAcademicPopUpCalenderYearArrowDropDownIcon().eq(2).click()
+        academicsetuppage.getAddAcademicPopUpCalenderYearArrowDropDownLists().contains('2025').click()
         academicsetuppage.getAddAcademicSetUpPopUpCalenderDates().contains('30').click()
         academicsetuppage.getAddAcademicPopUpAddBttn().click()
       })
 
-      it("admin school 002 Verify that School admin can add the Grades and subject successfully",function(){
+      it.only("admin school 002 Verify that School admin can add the Grades and subject successfully",function(){
         cy.wait(2000)
         adminschoolpage.getSchoolSideBarNavigationImg().trigger('mouseover').click()
         adminschoolpage.getAdminSchoolQuickLinkTittle().should('have.text',this.academicSetUp.AdminSchoolQuickLinkTittle)
@@ -49,6 +42,20 @@ describe("Admin School Validation", function () {
         adminschoolpage.getAcademicSetUpTittle().should('have.text',this.academicSetUp.academicSetUpTittle)
         cy.wait(2000)
         academicsetuppage.getGradeAndAcademicTab().click()
+        gradeAndSubjectPage.getGrades().each(($el,index,$List)=>{
+          var grades = $el.text()
+          if(grades === "Grade 5"){
+            gradeAndSubjectPage.getSectionAddBtn().eq(index).click()
+            gradeAndSubjectPage.getAddNewSectionPopUPSectionNameTextField().eq(0).type(this.academicSetUp.SectionName)
+            gradeAndSubjectPage.getAddNewSectionPopUPSectionNameTextField().eq(1).type(this.academicSetUp.NumberOfStudent)
+            gradeAndSubjectPage.getAddNewSectionPopUPSectionNameTextField().eq(2).click()
+            gradeAndSubjectPage.getAddNewSectionPopUPOptionalSubjectsLists().click({multiple:true})
+            cy.wait(2000)
+            cy.get('body').click({force:true})
+            gradeAndSubjectPage.getAddNewSectionPopUPAddSectionBtn()
+          }
+        })
+        
       })
     })
 
