927d876374695d40e1d271512534302e6d0ed67d
AkshayGowda52 <akshayga52@gmail.com>
2023-05-16 17:41:52 +0530

    commit ak

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,3 +3,12 @@
 
 
 /// <reference types = "Cypress" />
+
+const cypress = require("cypress");
+
+const adminschoolpage = require('../support/pageObjects/LMS/admin_Schoolpage.js')
+Cypress.Commands.add('NavigateToSchoolInfrastructure',()=>{
+    adminschoolpage.getSchoolSideBarNavigationImg().click()
+    adminschoolpage.getSchoolInfrastructures().click()
+
+})
